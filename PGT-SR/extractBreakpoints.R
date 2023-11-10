cdf1423639ed3ced2f7e57069847431148560209
Rebekka Koeck <94831697+rkoeck@users.noreply.github.com>
2023-11-10 10:20:31 +0100

    Create extractBreakpoints.R

---
+++ PGT-SR/extractBreakpoints.R
@@ -0,0 +1,12 @@
+###########################################################################################################################
+# Author: Rebekka Koeck
+# Lab: Cellular Genomic Medicine, Clinical Genetics, Maastricht University (Medical Centre +)
+
+# script purpose: extract relevant breakpoint information from the manta output
+
+# input: structural variant information (.vcf) - output from manta.sh
+
+# output: filtered structural variants per sample (.csv)
+
+###########################################################################################################################
+
